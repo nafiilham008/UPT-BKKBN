1661321412fd7a48568218aca407a00dcea4ca28
nafiilham_h <nafiilham.h@gmail.com>
2023-06-07 13:48:41 +0700

    add visitor count

---
+++ database/migrations/2023_06_06_131841_create_visitors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->integer('day_count')->default(0);
+            $table->integer('week_count')->default(0);
+            $table->integer('month_count')->default(0);
+            $table->integer('year_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitors');
+    }
+};
