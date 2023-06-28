2f0a03f8efe18197b01b62270358c96a3cbbe5e6
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-06-28 19:17:15 +0700

    showhide password in login register

--- resources/views/remaja/auth-user/login.blade.php
+++ resources/views/remaja/auth-user/login.blade.php
@@ -8,7 +8,7 @@
             <img src="../img/logo/logo-menjadi.svg" alt="" class="mb-9">
             <div class="rounded-[24px] bg-white/40 py-[22px] w-[522px] box-shadow">
                 <h1 class="font-be-vietnam text-[32px] font-semibold text-center mb-1.5 text-black">Login</h1>
-                <h1 class="font-be-vietnam text-xs font-medium text-center text-[#272727B2]/70">Enter your username and
+                <h1 class="font-be-vietnam text-xs font-medium text-center text-[#272727]/70">Enter your username and
                     password correctly</h1>
                 <div class="px-20 py-10">
                     <div class="mb-4">
@@ -33,8 +33,8 @@
                     <div class="mb-3">
                         <h1 class="font-be-vietnam text-xs font-semibold text-black mb-3">Password</h1>
                         <div class="relative">
-                            <input type="password"
-                                class="bg-white border border-[#5C7AEA] px-[18px] pl-10 py-3 w-full text-[#272727]/30 rounded-full font-be-vietnam text-xs"
+                            <input type="password" id="password" name="password"
+                                class="bg-white border border-[#5C7AEA] px-[18px] pl-10 pr-10 py-3 w-full text-[#272727]/30 rounded-full font-be-vietnam text-xs"
                                 placeholder="Password">
                             <div class="absolute top-2 left-3">
                                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -44,6 +44,13 @@
                                         fill="#3754C1" />
                                 </svg>
                             </div>
+                            <span id="password-toggle"
+                                class="absolute right-3 top-[22px] transform -translate-y-1/2 cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256">
+                                    <path fill="#5C7AEA"
+                                        d="M247.31 124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57 61.26 162.88 48 128 48S61.43 61.26 36.34 86.35C17.51 105.18 9 124 8.69 124.76a8 8 0 0 0 0 6.5c.35.79 8.82 19.57 27.65 38.4C61.43 194.74 93.12 208 128 208s66.57-13.26 91.66-38.34c18.83-18.83 27.3-37.61 27.65-38.4a8 8 0 0 0 0-6.5ZM128 192c-30.78 0-57.67-11.19-79.93-33.25A133.47 133.47 0 0 1 25 128a133.33 133.33 0 0 1 23.07-30.75C70.33 75.19 97.22 64 128 64s57.67 11.19 79.93 33.25A133.46 133.46 0 0 1 231.05 128c-7.21 13.46-38.62 64-103.05 64Zm0-112a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48Zm0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32Z" />
+                                </svg>
+                            </span>
                         </div>
                     </div>
                     <div class="flex justify-between mb-4">
@@ -53,7 +60,7 @@
                             <label for="pengantaranSelesai" class="cursor-pointer text-xs text-black">Save
                                 Remember me</label>
                         </div>
-                        <a href=""
+                        <a onclick="showLoading(event)" href="/forgot-password"
                             class="font-be-vietnam text-xs text-[#3754C1] hover:text-blue-700 transition-colors duration-300">Forgot
                             Password ?</a>
                     </div>
@@ -70,9 +77,9 @@
                             Sign In with Google
                         </button>
                     </div>
-                   <div class="flex justify-center gap-1 mb-10">
+                    <div class="flex justify-center gap-1 mb-10">
                         <h1 class="font-be-vietnam text-xs text-[#272727]">Don’t have an account?</h1>
-                        <a href="/register-user"
+                        <a onclick="showLoading(event)" href="/register-user"
                             class="font-be-vietnam text-xs text-[#3754C1] hover:text-blue-700 transition-colors duration-300 font-semibold">Sign
                             Up</a>
                     </div>
@@ -101,3 +108,16 @@
         </div>
     </div>
 @endsection
+@push('js')
+    <script>
+        const passwordInput = document.getElementById('password');
+        const passwordToggle = document.getElementById('password-toggle');
+        passwordToggle.addEventListener('click', () => {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+            passwordToggle.innerHTML = type === 'password' ?
+                `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="#5C7AEA" d="M247.31 124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57 61.26 162.88 48 128 48S61.43 61.26 36.34 86.35C17.51 105.18 9 124 8.69 124.76a8 8 0 0 0 0 6.5c.35.79 8.82 19.57 27.65 38.4C61.43 194.74 93.12 208 128 208s66.57-13.26 91.66-38.34c18.83-18.83 27.3-37.61 27.65-38.4a8 8 0 0 0 0-6.5ZM128 192c-30.78 0-57.67-11.19-79.93-33.25A133.47 133.47 0 0 1 25 128a133.33 133.33 0 0 1 23.07-30.75C70.33 75.19 97.22 64 128 64s57.67 11.19 79.93 33.25A133.46 133.46 0 0 1 231.05 128c-7.21 13.46-38.62 64-103.05 64Zm0-112a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48Zm0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32Z"/></svg>` :
+                `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 36 36"><path fill="#5C7AEA" d="M25.19 20.4a6.78 6.78 0 0 0 .43-2.4a6.86 6.86 0 0 0-6.86-6.86a6.79 6.79 0 0 0-2.37.43L18 13.23a4.78 4.78 0 0 1 .74-.06A4.87 4.87 0 0 1 23.62 18a4.79 4.79 0 0 1-.06.74Z" class="clr-i-outline clr-i-outline-path-1"/><path fill="#5C7AEA" d="M34.29 17.53c-3.37-6.23-9.28-10-15.82-10a16.82 16.82 0 0 0-5.24.85L14.84 10a14.78 14.78 0 0 1 3.63-.47c5.63 0 10.75 3.14 13.8 8.43a17.75 17.75 0 0 1-4.37 5.1l1.42 1.42a19.93 19.93 0 0 0 5-6l.26-.48Z" class="clr-i-outline clr-i-outline-path-2"/><path fill="#5C7AEA" d="m4.87 5.78l4.46 4.46a19.52 19.52 0 0 0-6.69 7.29l-.26.47l.26.48c3.37 6.23 9.28 10 15.82 10a16.93 16.93 0 0 0 7.37-1.69l5 5l1.75-1.5l-26-26Zm9.75 9.75l6.65 6.65a4.81 4.81 0 0 1-2.5.72A4.87 4.87 0 0 1 13.9 18a4.81 4.81 0 0 1 .72-2.47Zm-1.45-1.45a6.85 6.85 0 0 0 9.55 9.55l1.6 1.6a14.91 14.91 0 0 1-5.86 1.2c-5.63 0-10.75-3.14-13.8-8.43a17.29 17.29 0 0 1 6.12-6.3Z" class="clr-i-outline clr-i-outline-path-3"/><path fill="none" d="M0 0h36v36H0z"/></svg>`;
+        });
+    </script>
+@endpush
