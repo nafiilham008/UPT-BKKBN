9d2cc00f47d2c1367feccd4454c52379ba7a6e79
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-07-05 18:28:28 +0700

    navbar

--- resources/views/layouts/remaja/front/js.blade.php
+++ resources/views/layouts/remaja/front/js.blade.php
@@ -3,15 +3,35 @@
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 <script>
     function showLoading(event) {
-      event.preventDefault();
-      document.querySelector('.loading-overlay').classList.add('active');
-      setTimeout(function() {
-        window.location.href = event.target.href;
-      }, 1000);
+        event.preventDefault();
+        document.querySelector('.loading-overlay').classList.add('active');
+        setTimeout(function() {
+            window.location.href = event.target.href;
+        }, 1000);
     }
 
     // Menghilangkan overlay loading setelah halaman selesai dimuat
     window.addEventListener('load', function() {
-      document.querySelector('.loading-overlay').classList.remove('active');
+        document.querySelector('.loading-overlay').classList.remove('active');
+    });
+</script>
+<script type="text/javascript">
+    var nav = document.querySelector('nav');
+
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 100) {
+            nav.classList.add('bg-white', 'shadow');
+        } else {
+            nav.classList.remove('bg-white', 'shadow');
+        }
+    });
+</script>
+<script>
+    const button = document.querySelector('#menu-button');
+    const menu = document.querySelector('#menu');
+
+
+    button.addEventListener('click', () => {
+        menu.classList.toggle('hidden');
     });
 </script>
