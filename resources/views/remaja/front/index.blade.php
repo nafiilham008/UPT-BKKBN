318a90e0a6697f83e92911314f1a3f6a3034d5e1
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2023-08-10 14:33:04 +0700

    responsive landingpage remaja

--- resources/views/remaja/front/index.blade.php
+++ resources/views/remaja/front/index.blade.php
@@ -4,31 +4,32 @@
 
 @section('content')
     <div>
-        <div id="lottie-background"></div>
+        <div id="lottie-background" class="h-[500px]"></div>
         @include('layouts.remaja.component.navbar')
-        <div id="content" class="py-24">
-            <h1 class="font-titan text-[40px] text-[#272727]/70 mb-5">Let's Start The Game</h1>
-            <h1 class="font-be-vietnam text-base text-center text-[#272727]/70">Lorem ipsum dolor sit amet consectetur. Donec
+        <div id="content" class="lg:py-24 py-12 h-auto lg:px-0 px-5">
+            <h1 class="font-titan lg:text-[40px] text-2xl text-[#272727]/70 mb-5">Let's Start The Game</h1>
+            <h1 class="font-be-vietnam lg:text-base text-base text-center text-[#272727]/70">Lorem ipsum dolor sit amet
+                consectetur. Donec
                 eu
                 tortor quis mollis cursus sed<br>vestibulum nec sit. Iaculis id blandit ut in at urna.</h1>
-            <a href="{{ route('user.list') }}" class="mb-10 h-max w-max">
+            <a href="{{ route('user.list') }}" class="mb-10 w-64 h-64">
                 <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_nvkvb2j5.json" background="transparent"
-                    speed="1" style="width: 257px; height: 257px;" loop autoplay></lottie-player>
+                    speed="1" class="w-64 h-64" loop autoplay></lottie-player>
             </a>
-            <a href="" class="h-max w-max">
+            {{-- <a href="" class="h-max w-max">
                 <div
                     class="bg-[#FAA307] hover:bg-[#FFCA3A] transition-colors duration-300 py-4 px-[75px] font-be-vietnam font-medium text-base text-white rounded-[12px]">
                     Learn More
                 </div>
-            </a>
+            </a> --}}
         </div>
-        <div class="bg-white relative px-[200px] py-7">
+        <div class="bg-white relative lg:px-[200px] px-5 py-7">
             <div class="flex gap-8 items-center mb-16">
                 <div class="w-8 h-[73px] rounded-full bg-[#CF6EA7]"
                     style="box-shadow: -2px -1px 14px 0px rgba(133, 145, 255, 0.30);"></div>
-                <h1 class="text-[40px] text-[#272727] font-bold font-be-vietnam">About Menjadi Remaja</h1>
+                <h1 class="lg:text-[40px] text-[30px] text-[#272727] font-bold font-be-vietnam">About Menjadi Remaja</h1>
             </div>
-            <div class="grid grid-cols-3 gap-[30px] justify-center items-center mb-24">
+            <div class="lg:grid lg:grid-cols-3 lg:gap-[30px] flex flex-col lg:space-y-0 space-y-10 lg:flex-none justify-center items-center mb-24">
                 <div class="bg-[#B1DAF3] rounded-[24px] py-10 px-6 relative w-full"
                     style="box-shadow: -2px -1px 14px 0px rgba(133, 145, 255, 0.30);">
                     <div class="">
@@ -68,7 +69,7 @@
             <div class="flex gap-8 items-center mb-7">
                 <div class="w-8 h-[73px] rounded-full bg-[#CF6EA7]"
                     style="box-shadow: -2px -1px 14px 0px rgba(133, 145, 255, 0.30);"></div>
-                <h1 class="text-[40px] text-[#272727] font-bold font-be-vietnam">Menjadi Remaja</h1>
+                <h1 class="lg:text-[40px] text-[30px] text-[#272727] font-bold font-be-vietnam">Menjadi Remaja</h1>
             </div>
             <h1 class="text-lg text-[#212121] font-be-vietnam"><label class="font-bold">Menjadi Remaja</label> adalah sebuah
                 media yang
@@ -85,7 +86,7 @@
                 <img src="{{ asset('img/remaja/assets/book-right.svg') }}" alt="">
             </div>
         </div>
-        <div class="bg-[#EFF4FF] py-[70px] flex justify-center">
+        <div class="bg-[#EFF4FF] py-[70px] lg:px-0 px-5 flex justify-center">
             <div class="">
                 <h1 class="text-2xl text-black text-center font-semibold font-inter mb-4">Ayo coba main sekarang dan kenali
                     dirimu! </h1>
@@ -153,6 +154,23 @@
             flex-direction: column;
             align-items: center;
             justify-content: center;
+        }
+
+        lottie-player {
+            width: 257px;
+            height: 257px;
+        }
+
+        /* Style untuk layar dengan lebar maksimum 768px (misalnya mobile) */
+        @media only screen and (max-width: 769px) {
+            #lottie-background {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 600px;
+                z-index: -1;
+            }
         }
     </style>
 @endpush
