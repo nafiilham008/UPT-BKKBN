c1bc09e7b44c899605b97d4d3f77040a598912ba
nafiilham008 <nafiilham008@gmail.com>
2023-08-16 15:18:08 +0700

    add maintenance mode

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Maintenance;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Visitor;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
@@ -14,8 +16,21 @@ class DashboardController extends Controller
 
         $user = User::all();
 
-        return view('dashboard', compact('post', 'user'));
+        $maintenanceStatus = Maintenance::select('is_maintenance')->first();
+
+        return view('dashboard', compact('post', 'user', 'maintenanceStatus'));
     }
 
-    
+    public function updateMaintenance(Request $request)
+    {
+        $maintenanceStatus = $request->input('maintenance', 0);
+        // dd($maintenanceStatus);
+
+        Maintenance::updateOrCreate(
+            ['id' => 1],
+            ['is_maintenance' => $maintenanceStatus]
+        );
+
+        return redirect()->route('dashboard')->with('success', 'Maintenance mode is Active');
+    }
 }
