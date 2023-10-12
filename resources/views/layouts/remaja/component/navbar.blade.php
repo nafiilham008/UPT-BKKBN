c7622ad480cb415c6d10e7e15d70f111f8b1b8c0
Muhammad Iqbal Ainu Rafie <ainurafie@gmail.com>
2023-10-12 09:46:05 +0700

    logo

--- resources/views/layouts/remaja/component/navbar.blade.php
+++ resources/views/layouts/remaja/component/navbar.blade.php
@@ -2,8 +2,8 @@
     <nav
         class="flex flex-wrap items-center justify-between w-full py-4 md:py-3 lg:px-14 px-4 text-lg text-gray-700 bg-transparent">
         <div>
-            <a href="{{ route('user.index') }}">
-                <img src="{{ asset('img/remaja/logo/logo.svg') }}" alt="">
+            <a href="{{ route('user.index') }}" class="flex">
+                <img src="{{ asset('img/logo/logo_upt.png') }}" alt="" class="h-20">
             </a>
         </div>
 
@@ -16,15 +16,6 @@
 
             <ul
                 class="pt-4 text-base text-[#272727] font-be-vietnam md:flex  md:items-center md:justify-between md:pt-0">
-                @guest
-                    <li>
-                        <a class="px-[19px] font-semibold rounded-full py-2 border border-[#3754C1] text-green-500 items-center flex gap-3 min-w-[189px] justify-center"
-                            href="{{ route('remaja.login') }}" style="box-shadow: 2px 4px 17px 0px rgba(12, 0, 86, 0.25);">
-                            {{-- ketika belum login --}}
-                            <span>Sign</span>
-                        </a>
-                    </li>
-                @endguest
                 <li>
                     <a class="md:p-4 py-2 block hover:text-purple-400" href="{{ route('user.index') }}">Home</a>
                 </li>
@@ -34,8 +25,17 @@
                 </li>
                 <li>
                     <a class="md:p-4 py-2 block hover:text-purple-400" href="{{ route('user.detail.rangking') }}"
-                        data-href="{{ route('user.detail.rangking') }}" onclick="showLoading(event)">Ranking</a>
+                    data-href="{{ route('user.detail.rangking') }}" onclick="showLoading(event)">Ranking</a>
                 </li>
+                @guest
+                    <li>
+                        <a class="px-[19px] font-semibold rounded-full py-2 border border-[#3754C1] text-[#3754C1] items-center flex gap-3 min-w-[189px] justify-center"
+                            href="{{ route('remaja.login') }}" style="box-shadow: 2px 4px 17px 0px rgba(12, 0, 86, 0.25);">
+                            {{-- ketika belum login --}}
+                            <span>Sign</span>
+                        </a>
+                    </li>
+                @endguest
 
 
                 @auth
